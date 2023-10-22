3c503be84bd59afda42a3a41ba5a0583dd83e1e5
Alkis Pouros <alkispou35@gmail.com>
2023-10-22 15:36:43 +0300

    Updated React js project to React ts
    Linked frontend to backend

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
 
-app.get("/api", (req,res)=>{
-    res.json({"users": ["Alkis","Alex","and Co"]})
-})  //for testing purposes
 
-app.listen(5000, () => {console.log("Server started on port 5000")})
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function(req,res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
+/*
+app.get("/App.tsx", (req,res) => {
+    res.json({"users": ["Alkis","Alex","and co"]});
+})
+*/
+
+app.listen(5000, () => { console.log("Server started on port 5000") });
