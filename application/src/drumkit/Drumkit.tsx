5ea3c2984b1e735211251c94a6a1cecd539fb87a
Alex Mandelias <alexmandelias@gmail.com>
2023-11-15 16:15:04 +0200

    Adds documentation for the Drumkit components

--- application/src/drumkit/Drumkit.tsx
+++ application/src/drumkit/Drumkit.tsx
@@ -43,11 +43,40 @@ function Drumkit() {
     );
 }
 
-interface DrumkitGridProps {
+/**
+ * Defines the props for the DrumkitGrid component.
+ *
+ * @author Alex Mandelias
+ *
+ * @since v0.0.2
+ */
+type DrumkitGridProps = {
+
+    /**
+     * The DrumkitLogic instance which holds the loop data.
+     *
+     * @since v0.0.2
+     */
     dk: DrumkitLogic;
+
+    /**
+     * The instrument map used to construct the grid.
+     *
+     * @since v0.0.2
+     */
     instruments: Map<number, Instrument>;
 }
 
+/**
+ * Represents the whole Drumkit Grid component. It consists of one Drumkit Row
+ * component for every instrument.
+ *
+ * @author Alex Mandelias
+ *
+ * @since v0.0.2
+ *
+ * @see {@link DrumkitGridProps}
+ */
 function DrumkitGrid({ dk, instruments }: Readonly<DrumkitGridProps>) {
 
     // array of instrument ids, sorted from lowest to highest
@@ -67,11 +96,42 @@ function DrumkitGrid({ dk, instruments }: Readonly<DrumkitGridProps>) {
     )
 }
 
-interface DrumkitRowProps {
+/**
+ * Defines the props for the DrumkitRow component.
+ *
+ * @author Alex Mandelias
+ *
+ * @since v0.0.2
+ */
+type DrumkitRowProps = {
+
+    /**
+     * The DrumkitLogic instance which holds the loop data.
+     *
+     * @since v0.0.2
+     */
     dk: DrumkitLogic;
+
+    /**
+     * The instrument which corresponds to this row.
+     *
+     * @since v0.0.2
+     */
     instrument: Instrument;
 }
 
+/**
+ * Represents an instrument (a single row) in the Drumkit Grid component. It
+ * consists of one Drumkit Cell component for each tick in the drumkit loop.
+ * Each cell changes the state of the instrument on that tick according to its
+ * state.
+ *
+ * @author Alex Mandelias
+ *
+ * @since v0.0.2
+ *
+ * @see {@link DrumkitGridProps}
+ */
 function DrumkitRow({ dk, instrument }: Readonly<DrumkitRowProps>) {
 
     // array 0 - tickCount-1
@@ -97,10 +157,33 @@ function DrumkitRow({ dk, instrument }: Readonly<DrumkitRowProps>) {
 
 // TODO: contemplate whether beat info is required for the specific slot
 // maybe use beat to style column inside of loop?
-interface DrumkitCellProps {
+
+/**
+ * Defines the props for the DrumkitCell component.
+ *
+ * @author Alex Mandelias
+ *
+ * @since v0.0.2
+ */
+type DrumkitCellProps = {
+
+    /**
+     * The callback function which is called when the checked state of this
+     * cell is changed
+     *
+     * @since v0.0.2
+     */
     onCheckedChanged: (checked: boolean) => void;
 }
 
+/**
+ * Represents a single cell in the Drumkit Grid. When clicked, its state
+ * changes and the callback function is called.
+ *
+ * @author Alex Mandelias
+ *
+ * @since v0.0.2
+ */
 function DrumkitCell({ onCheckedChanged }: Readonly<DrumkitCellProps>) {
     const [checked, setChecked] = React.useState(false);
 
